5c4c368346b2b38c85d28f58f82d41803900dc83
cs alfred <csalfred16@gmail.com>
2026-09-11 12:10:47 +0530

    created the first agent named researcher

--- agent.py
+++ agent.py
@@ -1,2 +1,14 @@
 from crewai import Agent,Task,Crew
 
+
+researcher = Agent(role = "You are a digital Research Specialist in Emerging Technologies", 
+                   goal = "Discover and analyze the latest advancements in emerging technologies." 
+                          "Provide insights and recommendations for future research directions.",  
+                   backstory = "You have a background in computer science and library science." 
+                                "You have centuries of experience in research in the field of emerging technology."    
+                                "You have mastered the art of digital research." 
+                                "You have also worked with industry leaders in tech and also with research faculties at prestigious universities.", 
+                                allow_delegation = False, 
+                                verbose = True
+
+)
