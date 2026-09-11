4f82d7609c0a9accfe944177a26caa177900405b
cs alfred <csalfred16@gmail.com>
2026-09-11 16:18:26 +0530

    completed the creation of 3 agents.

--- agent.py
+++ agent.py
@@ -2,8 +2,8 @@ from crewai import Agent,Task,Crew
 
 
 researcher = Agent(role = "You are a digital Research Specialist in Emerging Technologies", 
-                   goal = "Discover and analyze the latest advancements in emerging technologies." 
-                          "Provide insights and recommendations for future research directions.",  
+                   goal = "You Discover and analyze the latest advancements in emerging technologies." 
+                          "You Provide insights and recommendations for future research directions.",  
                    backstory = "You have a background in computer science and library science." 
                                 "You have centuries of experience in research in the field of emerging technology."    
                                 "You have mastered the art of digital research." 
@@ -14,4 +14,28 @@ researcher = Agent(role = "You are a digital Research Specialist in Emerging Tec
 )
 
 
-summarizer = ""
+summarizer = Agent(role = "You are an editor and summarizer of research findings.",
+                   goal = "You summarize and edit research findings into concise and clear points.", 
+                   backstory = "You are an expert in summarizing complex research findings into clear and concise points."
+                               "You have a background in technical writing and editing."
+                               "You have centuries of experience in summarizing research findings for various audiences."
+                               "You have also worked with industry leaders in tech and also with research faculties at prestigious universities.",
+                         allow_delegation = False,
+                         verbose = True
+
+
+
+
+)
+
+
+dispatcher = Agent(role = "You are a dispatcher of research findings.",
+                   goal = "You will send the summarized research findings to the relevant recipient",
+                   backstory = "You are an expert in dispatching research findings to the relevant recipients."
+                               "You have a background in logistics and communication."
+                               "You have centuries of experience in dispatching research findings to various audiences."
+                               "You have also worked with industry leaders in tech and also with research faculties at prestigious universities.",
+                         allow_delegation = False,
+                         verbose = True
+
+)
