3d7e9ea7fb01d469bc87f2e7977f9b7d4ac2a048
cs alfred <csalfred16@gmail.com>
2026-09-14 15:49:56 +0530

    .

--- agent.py
+++ agent.py
@@ -9,7 +9,7 @@ load_dotenv()
 search_tool = SerperDevTool()
 
 my_llm = LLM(
-    model="Gemini 2.5 Flash Lite",
+    model="gemini/gemini-2.5-flash-lite",
     api_key=os.getenv("GEMINI_API_KEY")
 )
 
